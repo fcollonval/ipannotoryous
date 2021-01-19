3513c809603514af8b89e0027a8bd325d41a2b72
Frédéric Collonval <frederic.collonval@ariadnext.com>
2021-01-19 17:58:07 +0100

    Install as nbextension in binder

--- binder/postBuild.sh
+++ binder/postBuild.sh
@@ -3,6 +3,8 @@
 set -e
 jlpm install
 jlpm run build
-python -m pip install .
+python -m pip install -e .
+jupyter nbextension install --sys-prefix --symlink --overwrite --py ipannotoryous
+jupyter nbextension enable --sys-prefix --py ipannotoryous
 # jupyter labextension install @jupyter-widgets/jupyterlab-manager@2.0 . --no-build
 # jupyter lab build --minimize=False
